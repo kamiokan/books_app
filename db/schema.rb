a4256473081056cd0a059b74c6ae69a591ecd128
kamiokan <spherenov@gmail.com>
2022-02-08 09:00:46 +0900

    booksテーブルにauthorカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_140957) do
+ActiveRecord::Schema.define(version: 2022_02_07_235100) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
